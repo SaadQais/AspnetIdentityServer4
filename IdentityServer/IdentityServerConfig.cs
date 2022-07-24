593d3e2431a27e5ef3ed19c24918cdce0268d4df
Saad Qais <saadqais2013@live.com>
2022-07-24 09:55:01 +0300

    add test user ti is4

--- IdentityServer/IdentityServerConfig.cs
+++ IdentityServer/IdentityServerConfig.cs
@@ -1,5 +1,8 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
 using IdentityServer4.Test;
+using System.Security.Claims;
 
 namespace IdentityServer
 {
@@ -15,15 +18,67 @@ namespace IdentityServer
                     new Secret("secret".Sha256())
                 },
                 AllowedScopes = { "MoviesApi" }
-            } 
+            },
+            new Client
+            {
+                ClientId = "movies_mvc_client",
+                ClientName = "Movies MVC Web App",
+                AllowedGrantTypes = GrantTypes.Hybrid,
+                RequirePkce = false,
+                AllowRememberConsent = false,
+                RedirectUris = new List<string>()
+                {
+                    "https://localhost:5002/signin-oidc"
+                },
+                PostLogoutRedirectUris = new List<string>()
+                {
+                    "https://localhost:5002/signout-callback-oidc"
+                },
+                ClientSecrets = new List<Secret>
+                {
+                    new Secret("secret".Sha256())
+                },
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    IdentityServerConstants.StandardScopes.Address,
+                    IdentityServerConstants.StandardScopes.Email,
+                    "MoviesApi",
+                    "roles"
+                }
+            }
         };
 
         public static IEnumerable<ApiScope> ApiScopes => new ApiScope[] { new ApiScope("MoviesApi", "Movies Api") };
 
         public static IEnumerable<ApiResource> ApiResources => new ApiResource[] { };
 
-        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[] { };
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[] 
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResources.Address(),
+            new IdentityResources.Email(),
+            new IdentityResource(
+                "roles",
+                "Your role(s)",
+                new List<string>() { "role" })
+        };
 
-        public static List<TestUser> TestUsers => new() { };
+        public static List<TestUser> TestUsers => new() 
+        {
+            new TestUser
+            {
+                SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
+                Username = "saad",
+                Password = "123qweASD",
+                Claims = new List<Claim>
+                {
+                    new Claim(JwtClaimTypes.GivenName, "saad"),
+                    new Claim(JwtClaimTypes.FamilyName, "qais")
+                }
+            }
+        };
     }
 }
--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -2,15 +2,26 @@ using IdentityServer;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllersWithViews();
+
 builder.Services.AddIdentityServer()
     .AddInMemoryClients(IdentityServerConfig.Clients)
     .AddInMemoryApiScopes(IdentityServerConfig.ApiScopes)
+    .AddInMemoryIdentityResources(IdentityServerConfig.IdentityResources)
+    .AddTestUsers(IdentityServerConfig.TestUsers)
     .AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+app.UseRouting();
 app.UseIdentityServer();
 
-app.MapGet("/", () => "Hello World!");
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
 
 app.Run();
