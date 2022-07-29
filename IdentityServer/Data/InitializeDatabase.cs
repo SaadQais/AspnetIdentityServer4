1884add3b559b87949d5bc0745679d43044c7de6
Saad Qais <saadqais2013@live.com>
2022-07-29 21:24:44 +0300

    migrate from InMemeoryDB to sql server db

---
+++ IdentityServer/Data/InitializeDatabase.cs
@@ -0,0 +1,52 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer.Data
+{
+    public class InitializeDatabase
+    {
+        public static void Seed(IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope();
+
+            serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+            var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+
+            context.Database.Migrate();
+
+            //Add Clients
+            if (!context.Clients.Any())
+            {
+                foreach (var client in IdentityServerConfig.Clients)
+                {
+                    context.Clients.Add(client.ToEntity());
+                }
+
+                context.SaveChanges();
+            }
+
+            //Add Identity Resources
+            if (!context.IdentityResources.Any())
+            {
+                foreach (var resource in IdentityServerConfig.IdentityResources)
+                {
+                    context.IdentityResources.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+
+            //Add Api Scopes
+            if (!context.ApiScopes.Any())
+            {
+                foreach (var resource in IdentityServerConfig.ApiScopes)
+                {
+                    context.ApiScopes.Add(resource.ToEntity());
+                }
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -1,18 +1,40 @@
-using IdentityServer;
+using IdentityServer.Data;
 using IdentityServerHost.Quickstart.UI;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var migrationsAssembly = typeof(Program).GetTypeInfo().Assembly.GetName().Name;
+const string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;database=IdentityServer4.Quickstart.EntityFramework-4.0.0;trusted_connection=yes;";
+
+
 builder.Services.AddControllersWithViews();
 
+//builder.Services.AddIdentityServer()
+//    .AddInMemoryClients(IdentityServerConfig.Clients)
+//    .AddInMemoryApiScopes(IdentityServerConfig.ApiScopes)
+//    .AddInMemoryIdentityResources(IdentityServerConfig.IdentityResources)
+//    .AddTestUsers(TestUsers.Users)
+//    .AddDeveloperSigningCredential();
+
 builder.Services.AddIdentityServer()
-    .AddInMemoryClients(IdentityServerConfig.Clients)
-    .AddInMemoryApiScopes(IdentityServerConfig.ApiScopes)
-    .AddInMemoryIdentityResources(IdentityServerConfig.IdentityResources)
     .AddTestUsers(TestUsers.Users)
+    .AddConfigurationStore(options =>
+    {
+        options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+            sql => sql.MigrationsAssembly(migrationsAssembly));
+    })
+    .AddOperationalStore(options =>
+    {
+        options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+            sql => sql.MigrationsAssembly(migrationsAssembly));
+    })
     .AddDeveloperSigningCredential();
 
 var app = builder.Build();
+
+InitializeDatabase.Seed(app);
 
 app.UseStaticFiles();
 app.UseRouting();
